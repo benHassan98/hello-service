a62d781f7f6c9187357da16f0816422f925cbea7
benhassan98 <habdala70@gmail.com>
2023-12-17 16:46:24 +0200

    to k8s w/o rabbitmq and mysql

--- src/main/java/com/example/helloservice/HelloServiceApplication.java
+++ src/main/java/com/example/helloservice/HelloServiceApplication.java
@@ -1,8 +1,8 @@
 package com.example.helloservice;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
+//import jakarta.persistence.EntityManager;
+//import jakarta.persistence.PersistenceContext;
+//import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -18,10 +18,10 @@ import java.util.List;
 public class HelloServiceApplication {
     @Value("${test.url}")
     private String t;
-    @PersistenceContext
-    private EntityManager entityManager;
-    @Autowired
-    private RabbitAdmin rabbitAdmin;
+//    @PersistenceContext
+//    private EntityManager entityManager;
+//    @Autowired
+//    private RabbitAdmin rabbitAdmin;
 
     public static void main(String[] args) {
         SpringApplication.run(HelloServiceApplication.class, args);
@@ -29,11 +29,12 @@ public class HelloServiceApplication {
 
     @GetMapping("/")
     public String hello(){
-        List<Object[]> objects = entityManager.createNativeQuery("select * from accounts").getResultList();
-        var x =  rabbitAdmin.getQueueInfo("odinBook.accountChannel");
-
-
-        return t+" "+objects.get(0)[0]+" "+x.getName();
+//        List<Object[]> objects = entityManager.createNativeQuery("select * from accounts").getResultList();
+//        var x =  rabbitAdmin.getQueueInfo("odinBook.accountChannel");
+//
+//
+//        return t+" "+objects.get(0)[0]+" "+x.getName();
+        return t+" Hello";
     }
 
 }
